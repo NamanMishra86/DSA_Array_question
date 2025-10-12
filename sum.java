74a995632ab7176e96641c3f1b1a8591db3c9d37
namanmishra86 <namanmishra8650@gmail.com>
2025-10-12 22:24:46 +0530

    add new file

---
+++ sum.java
@@ -0,0 +1,15 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println("a "+a+"b "+b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+        System.out.println("swapped "+a+"&"+b);
+
+    }
+    
+}
