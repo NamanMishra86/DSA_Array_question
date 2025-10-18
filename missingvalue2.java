28ef7ad35bd41639db34d1562a6dd7b96ebc06eb
namanmishra86 <namanmishra8650@gmail.com>
2025-10-18 22:32:02 +0530

    add newfile

---
+++ missingvalue2.java
@@ -0,0 +1,18 @@
+
+public class missingvalue2 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,5};
+
+        int n= arr.length + 1;
+        int total= n*(n+1)/2;
+        int sum = 0;
+
+        for( int num:arr){
+            sum+= num;
+
+        }
+        int missing= total-sum;
+        System.out.println("missing number"+ missing);
+    }
+    
+}
