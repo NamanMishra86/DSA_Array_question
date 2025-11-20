d36f4cf1f66787520a4ce14d93fc8fa13c86c350
namanmishra86 <namanmishra8650@gmail.com>
2025-11-20 23:23:03 +0530

    add new file

---
+++ factorai.java
@@ -0,0 +1,17 @@
+public class factorai {
+    public static void main(String[] args){
+        int num=10;
+        long factoraial=1;
+
+        for(int i=1;i<=num;i++){
+            factoraial=factoraial*i;
+
+        }
+        System.out.println("factorail oa a number is... "+factoraial);
+
+        
+
+        
+    }
+    
+}
