be6bd7d2320b495e851f305c048aeefaf2867ddb
namanmishra86 <namanmishra8650@gmail.com>
2025-12-08 01:15:25 +0530

    add new

--- reverse.java
+++ reverse.java
@@ -6,11 +6,15 @@ public class reverse {
         System.out.println("Enter the number");
         int num=sc.nextInt();
 
-        int rev=0;
-        while(num!=0){
-            rev=rev*10+num%10;
-            num=num/10;
-        }
+        // int rev=0;
+        // while(num!=0){
+        //     rev=rev*10+num%10;
+        //     num=num/10;
+        // }
+
+        StringBuffer rev;
+        StringBuffer sb=new StringBuffer(String.valueof(num));
+        rev = sb.reverse();
         System.out.println("after reverse number"+rev);
     }
     
