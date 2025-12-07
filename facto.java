e2156933dcdb52df2c9c2f8a2d8af25064ee58e6
namanmishra86 <namanmishra8650@gmail.com>
2025-12-07 21:39:10 +0530

    add new

---
+++ facto.java
@@ -0,0 +1,28 @@
+// public class factorai {
+//     public static void main(String[] args){
+//         int num=10;
+//         long factoraial=1;
+
+//         for(int i=1;i<=num;i++){
+//             factoraial=factoraial*i;
+
+//         }
+//         System.out.println("factorail oa a number is... "+factoraial);
+
+        
+
+        
+//     }
+    
+// }
+
+public class facto{
+    public static void main (String []args){
+        int num=20;
+        long factorial=1;
+        for(int i=1;i<=num;i++){
+            factorial =factorial*i;
+        }
+        System.out.println("factorial of a number is..."+factorial);
+    }
+}
--- fibonic.java
+++ fibonic.java
@@ -1,15 +1,32 @@
-public class fibonic {
-    public static void main(String[] args) {
-        for (int i = 0; i < 1; i++) {   
-            int a = 0;
-            int b = 1;
-            while (b <= 55) {           
+// public class fibonic {
+//     public static void main(String[] args) {
+//         for (int i = 0; i < 1; i++) {   
+//             int a = 0;
+//             int b = 1;
+//             while (b <= 55) {           
+//                 System.out.println(b);
+//                 int temp = a + b;       
+//                 a = b;
+//                 b = temp;
+//             }
+//         }
+//     }
+// }
+
+public class fibonic{
+    public static void main (String[] args){
+        for(int i=0;i<1;i++){
+            int a=0;
+            int b=1;
+            while(b<=55){
                 System.out.println(b);
-                int temp = a + b;       
-                a = b;
-                b = temp;
+                int temp=a+b;
+                a=b;
+                b=temp;
             }
         }
     }
 }
+
+
 
