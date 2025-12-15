a1268087c217251dfd24d74c22365c0c848713a9
namanmishra86 <namanmishra8650@gmail.com>
2025-12-15 22:18:51 +0530

    add new

---
+++ STr/MyString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MyString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the name: ");
+        String str = sc.nextLine();
+
+        System.out.println("Entered name is: " + str);
+    }
+}
