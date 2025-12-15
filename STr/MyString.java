e0ff21df94be612eb6a1a22fc34f59c6dada9c09
namanmishra86 <namanmishra8650@gmail.com>
2025-12-16 00:53:37 +0530

    add new

--- STr/MyString.java
+++ STr/MyString.java
@@ -1,12 +1,28 @@
+// import java.util.*;
+
+// public class MyString {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the name: ");
+//         String str = sc.nextLine();
+
+//         System.out.println("Entered name is: " + str);
+//     }
+// }
 import java.util.*;
+public class MyString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        // concatenation of Strings
+        String Firstname="naman";
+        String Lastname="mishra";
+        String Fullname=Firstname + " " + Lastname;
+        System.out.println(Fullname.length());
+        // charAT 
+        for(int i=0;i<Fullname.length();i++){
+            System.out.println(Fullname.charAt(i));
+        }
 
-public class MyString {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the name: ");
-        String str = sc.nextLine();
-
-        System.out.println("Entered name is: " + str);
     }
 }
