b004464b3f0d6ee848bd541e3251a682198131c3
namanmishra86 <namanmishra8650@gmail.com>
2025-12-16 22:33:22 +0530

    add new

---
+++ STr/Parsint.java
@@ -0,0 +1,8 @@
+public class Parsint {
+    public static void main(String[]args){
+        String str="1234";
+        int num=Integer.parseInt(str);
+        System.out.println("The number is "+num);
+    }
+    
+}
